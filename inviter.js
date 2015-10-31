49121f0118e3509dd6de6bc31e225c5f220f8bc1
Leonard Göhrs <leonard@goehrs.eu>
2015-10-31 23:20:01 +0100

    Binomial distribution svg generator

--- inviter.js
+++ inviter.js
@@ -1,6 +1,100 @@
 var event={}
 var guest={}
 
+function binomialDist(num, prop)
+{
+  function binomialCoef (n,k) {
+    its++;
+    if (k==0 || n==k) return 1;
+
+    return (binomialCoef(n-1,k-1) + binomialCoef(n-1,k));
+  }
+
+  var res=[];
+
+  for (var i=0; i<=num; i++) {
+    var tmp=1;
+    tmp*= binomialCoef(num, i);
+    tmp*= Math.pow(prop, i);
+    tmp*= Math.pow(1-prop, num-i);
+
+    res[i]=tmp;
+  }
+
+  return (res)
+}
+
+function genDistSVG (num, prop)
+{
+  var ns= 'http://www.w3.org/2000/svg'
+
+  function line (x1, y1, x2, y2, color) {
+    var line= document.createElementNS(ns, 'line');
+
+    line.setAttribute('x1',  x1 + '%');
+    line.setAttribute('x2',  x2 + '%');
+    
+    line.setAttribute('y1',  y1 + '%');
+    line.setAttribute('y2',  y2 + '%');
+
+    line.setAttribute('stroke', color);
+    line.setAttribute('stroke-width', '1');
+
+    return (line);
+  }
+
+  function text(xp, yp, txt) {
+    var text= document.createElementNS(ns, 'text');
+
+    text.setAttribute('x', xp + '%');
+    text.setAttribute('y', yp + '%');
+    text.setAttribute('fill', 'grey');
+
+    text.setAttribute('alignment-baseline', 'middle');
+    text.setAttribute('text-anchor', 'middle');
+    text.setAttribute('font-size', '0.7em');
+    
+    text.appendChild(document.createTextNode(txt));
+    
+    return (text);
+  }
+  
+  var svg= document.createElementNS(ns, 'svg');
+  svg.setAttribute('id', 'histogram');
+  svg.appendChild(line(2, 10,  2, 90, 'grey'));
+  svg.appendChild(line(2, 90, 98, 90, 'grey'));
+
+  var dist= binomialDist(num, prop);
+  var maxp=0;
+
+  for (var i=0; i<=num; i++) {
+    if (dist[i]>maxp) maxp=dist[i];
+  }
+  
+  for (var i=0; i<num; i++) {
+    var x1=(i/num)*96+2;
+    var x2=((i+1)/num)*96+2;
+
+    var y1=90 - dist[i]/maxp * 80;
+    var y2=90 - dist[i+1]/maxp * 80;
+
+    var lne= line(x1, y1, x2, y2, 'blue');
+
+    svg.appendChild(lne);
+  }
+
+  for (var i=0; i<=num; i++) {
+    var xp=(i/num)*96+2;
+
+    var txt= text(xp, 97.5, i);
+
+    svg.appendChild(txt);
+  }
+
+
+  return (svg)
+}
+
 function loadEvent ()
 {
   var gtoken= window.location.hash.substring(1, 9);
